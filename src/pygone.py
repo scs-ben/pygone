0411a0ba8e63221c8a9e91e2b586a64420d18561
Ben <breese@scsgrandrapids.com>
2020-09-23 08:42:29 -0400

    revert to master

--- src/pygone.py
+++ src/pygone.py
@@ -1,119 +1,4 @@
 #!/usr/bin/env pypy3
-<<<<<<< HEAD
-import gc, math, sys, time
-import itertools
-import re
-from itertools import count
-from collections import namedtuple
-
-piece = { 'P': 100, 'N': 280, 'B': 320, 'R': 479, 'Q': 929, 'K': 60000 }
-pst = {
-    'P': (   0,   0,   0,   0,   0,   0,   0,   0,
-            78,  83,  86,  73, 102,  82,  85,  90,
-             7,  29,  21,  44,  40,  31,  44,   7,
-           -17,  16,  -2,  15,  14,   0,  15, -13,
-           -26,   3,  10,   9,   6,   1,   0, -23,
-           -22,   9,   5, -11, -10,  -2,   3, -19,
-           -31,   8,  -7, -37, -36, -14,   3, -31,
-             0,   0,   0,   0,   0,   0,   0,   0),
-    'N': ( -66, -53, -75, -75, -10, -55, -58, -70,
-            -3,  -6, 100, -36,   4,  62,  -4, -14,
-            10,  67,   1,  74,  73,  27,  62,  -2,
-            24,  24,  45,  37,  33,  41,  25,  17,
-            -1,   5,  31,  21,  22,  35,   2,   0,
-           -18,  10,  13,  22,  18,  15,  11, -14,
-           -23, -15,   2,   0,   2,   0, -23, -20,
-           -74, -23, -26, -24, -19, -35, -22, -69),
-    'B': ( -59, -78, -82, -76, -23,-107, -37, -50,
-           -11,  20,  35, -42, -39,  31,   2, -22,
-            -9,  39, -32,  41,  52, -10,  28, -14,
-            25,  17,  20,  34,  26,  25,  15,  10,
-            13,  10,  17,  23,  17,  16,   0,   7,
-            14,  25,  24,  15,   8,  25,  20,  15,
-            19,  20,  11,   6,   7,   6,  20,  16,
-            -7,   2, -15, -12, -14, -15, -10, -10),
-    'R': (  35,  29,  33,   4,  37,  33,  56,  50,
-            55,  29,  56,  67,  55,  62,  34,  60,
-            19,  35,  28,  33,  45,  27,  25,  15,
-             0,   5,  16,  13,  18,  -4,  -9,  -6,
-           -28, -35, -16, -21, -13, -29, -46, -30,
-           -42, -28, -42, -25, -25, -35, -26, -46,
-           -53, -38, -31, -26, -29, -43, -44, -53,
-           -30, -24, -18,   5,  -2, -18, -31, -32),
-    'Q': (   6,   1,  -8,-104,  69,  24,  88,  26,
-            14,  32,  60, -10,  20,  76,  57,  24,
-            -2,  43,  32,  60,  72,  63,  43,   2,
-             1, -16,  22,  17,  25,  20, -13,  -6,
-           -14, -15,  -2,  -5,  -1, -10, -20, -22,
-           -30,  -6, -13, -11, -16, -11, -16, -27,
-           -36, -18,   0, -19, -15, -15, -21, -38,
-           -39, -30, -31, -13, -31, -36, -34, -42),
-    'K': (   4,  54,  47, -99, -99,  60,  83, -62,
-           -32,  10,  55,  56,  56,  55,  10,   3,
-           -62,  12, -57,  44, -67,  28,  37, -31,
-           -55,  50,  11,  -4, -19,  13,   0, -49,
-           -55, -43, -52, -28, -51, -47,  -8, -50,
-           -47, -42, -43, -79, -64, -32, -29, -32,
-            -4,   3, -14, -50, -57, -18,  13,   4,
-            17,  30,  -3, -14,   6,  -1,  40,  18),
-}
-# Pad tables and join piece and pst dictionaries
-for k, table in pst.items():
-    padrow = lambda row: (0,) + tuple(x+piece[k] for x in row) + (0,)
-    pst[k] = sum((padrow(table[i*8:i*8+8]) for i in range(8)), ())
-    pst[k] = (0,)*20 + pst[k] + (0,)*20
-
-# Lists of possible moves for each piece type.
-N, E, S, W = -10, 1, 10, -1
-directions = {
-    'P': (N, N+N, N+W, N+E),
-    'N': (N+N+E, E+N+E, E+S+E, S+S+E, S+S+W, W+S+W, W+N+W, N+N+W),
-    'B': (N+E, S+E, S+W, N+W),
-    'R': (N, E, S, W),
-    'Q': (N, E, S, W, N+E, S+E, S+W, N+W),
-    'K': (N, E, S, W, N+E, S+E, S+W, N+W)
-}
-
-# Mate value must be greater than 8*queen + 2*(rook+knight+bishop)
-# King value is set to twice this value such that if the opponent is
-# 8 queens up, but we got the king, we still exceed MATE_VALUE.
-# When a MATE is detected, we'll set the score to MATE_UPPER - plies to get there
-# E.g. Mate in 3 will be MATE_UPPER - 6
-MATE_LOWER = piece['K'] - 10*piece['Q']
-MATE_UPPER = piece['K'] + 10*piece['Q']
-
-###############################################################################
-# Global constants
-###############################################################################
-
-# Our board is represented as a 120 character string. The padding allows for
-# fast detection of moves that don't stay within the board.
-A1, H1, A8, H8 = 91, 98, 21, 28
-initial = (
-    '         \n'  #   0 -  9
-    '         \n'  #  10 - 19
-    ' rnbqkbnr\n'  #  20 - 29
-    ' pppppppp\n'  #  30 - 39
-    ' ........\n'  #  40 - 49
-    ' ........\n'  #  50 - 59
-    ' ........\n'  #  60 - 69
-    ' ........\n'  #  70 - 79
-    ' PPPPPPPP\n'  #  80 - 89
-    ' RNBQKBNR\n'  #  90 - 99
-    '         \n'  # 100 -109
-    '         \n'  # 110 -119
-)
-
-WHITE_PIECES = ['P', 'R', 'N', 'B', 'Q', 'K']
-BLACK_PIECES = ['p', 'r', 'n', 'b', 'q', 'k']
-
-TTEXACT = 1
-TTLOWER = 2
-TTUPPER = 3
-
-isupper = lambda c: 'A' <= c <= 'Z'
-islower = lambda c: 'a' <= c <= 'z'
-=======
 import copy, math, sys, time
 
 PIECEPOINTS = {'p': 100, 'r': 480, 'n': 280, 'b': 320, 'q': 960, 'k': 6e4}
@@ -171,7 +56,6 @@ ALLPSQT = {'p': PPSQT, 'n': NPSQT, 'b':BPSQT, 'r':RPSQT, 'q':QPSQT, 'k':KPSQT}
 
 WHITE_PIECES = ['P', 'R', 'N', 'B', 'Q', 'K']
 BLACK_PIECES = ['p', 'r', 'n', 'b', 'q', 'k']
->>>>>>> master
 
 def letter_to_number(letter):
     return abs((ord(letter) - 96) - 1)
@@ -180,149 +64,11 @@ def number_to_letter(number):
     return chr(number + 96)
 
 def print_to_terminal(letter):
-<<<<<<< HEAD
-    print(letter, flush=1)
-=======
     print(letter, flush=True)
->>>>>>> master
 
 def get_perf_counter():
     return time.perf_counter()
 
-<<<<<<< HEAD
-class Position(namedtuple('Position', 'board score wc bc ep kp')):
-    """ A state of a chess game
-    board -- a 120 char representation of the board
-    score -- the board evaluation
-    wc -- the castling rights, [west/queen side, east/king side]
-    bc -- the opponent castling rights, [west/king side, east/queen side]
-    ep - the en passant square
-    kp - the king passant square
-    """
-
-    def gen_moves(self):
-        # For each of our pieces, iterate through each possible 'ray' of moves,
-        # as defined in the 'directions' map. The rays are broken e.g. by
-        # captures or immediately in case of pieces such as knights.
-        for i, p in enumerate(self.board):
-            if not p.isupper(): continue
-            for d in directions[p]:
-                for j in count(i+d, d):
-                    q = self.board[j]
-                    # Stay inside the board, and off friendly pieces
-                    if q.isspace() or q.isupper(): break
-                    # Pawn move, double move and capture
-                    if p == 'P' and d in (N, N+N) and q != '.': break
-                    if p == 'P' and d == N+N and (i < A1+N or self.board[i+N] != '.'): break
-                    if p == 'P' and d in (N+W, N+E) and q == '.' \
-                            and j not in (self.ep, self.kp, self.kp-1, self.kp+1): break
-                    # Move it
-                    yield (i, j)
-                    # Stop crawlers from sliding, and sliding after captures
-                    if p in 'PNK' or q.islower(): break
-                    # Castling, by sliding the rook next to the king
-                    if i == A1 and self.board[j+E] == 'K' and self.wc[0]: yield (j+E, j+W)
-                    if i == H1 and self.board[j+W] == 'K' and self.wc[1]: yield (j+W, j+E)
-
-    def rotate(self):
-        ''' Rotates the board, preserving enpassant '''
-        return Position(
-            self.board[::-1].swapcase(), -self.score, self.bc, self.wc,
-            119-self.ep if self.ep else 0,
-            119-self.kp if self.kp else 0)
-
-    def nullmove(self):
-        ''' Like rotate, but clears ep and kp '''
-        return Position(
-            self.board[::-1].swapcase(), -self.score,
-            self.bc, self.wc, 0, 0)
-
-    def move(self, move):
-        i, j = move
-        p, q = self.board[i], self.board[j]
-        put = lambda board, i, p: board[:i] + p + board[i+1:]
-        # Copy variables and reset ep and kp
-        board = self.board
-        wc, bc, ep, kp = self.wc, self.bc, 0, 0
-        score = self.score + self.value(move)
-        # Actual move
-        board = put(board, j, board[i])
-        board = put(board, i, '.')
-        # Castling rights, we move the rook or capture the opponent's
-        if i == A1: wc = (False, wc[1])
-        if i == H1: wc = (wc[0], False)
-        if j == A8: bc = (bc[0], False)
-        if j == H8: bc = (False, bc[1])
-        # Castling
-        if p == 'K':
-            wc = (False, False)
-            if abs(j-i) == 2:
-                kp = (i+j)//2
-                board = put(board, A1 if j < i else H1, '.')
-                board = put(board, kp, 'R')
-        # Pawn promotion, double move and en passant capture
-        if p == 'P':
-            if A8 <= j <= H8:
-                board = put(board, j, 'Q')
-            if j - i == 2*N:
-                ep = i + N
-            if j == self.ep:
-                board = put(board, j+S, '.')
-        # We rotate the returned position, so it's ready for the next player
-        return Position(board, score, wc, bc, ep, kp).rotate()
-
-    def value(self, move):
-        i, j = move
-        p, q = self.board[i], self.board[j]
-        # Actual move
-        score = pst[p][j] - pst[p][i]
-        # Capture
-        if q.islower():
-            score += pst[q.upper()][119-j]
-        # Castling check detection
-        if abs(j-self.kp) < 2:
-            score += pst['K'][119-j]
-        # Castling
-        if p == 'K' and abs(i-j) == 2:
-            score += pst['R'][(i+j)//2]
-            score -= pst['R'][A1 if j < i else H1]
-        # Special pawn stuff
-        if p == 'P':
-            if A8 <= j <= H8:
-                score += pst['Q'][j] - pst['P'][j]
-            if j == self.ep:
-                score += pst['P'][119-(j+S)]
-        return score
-
-class Search:
-    nodes = 0
-    end_time = 0
-    v_depth = 0
-    tt_bucket = {}
-
-    def iterative_search(self, position, v_depth, move_time):
-        start_time = time.perf_counter()
-        self.end_time = time.perf_counter() + move_time
-
-        self.v_depth = 0
-
-        local_score = -1e8
-        local_move = None
-
-        while True:
-            self.v_depth += 1
-            v_depth -= 1
-
-            (iterative_score, iterative_move) = self.first_search(position, self.v_depth)
-
-            local_score = iterative_score
-            local_move = mrender(position, iterative_move)
-
-            elapsed_time = math.ceil(get_perf_counter() - start_time)
-            nps = math.ceil(self.v_nodes / elapsed_time)
-
-            print_to_terminal("info depth " + str(self.v_depth) + " score cp " + str(math.ceil(local_score)) + " time " + str(elapsed_time) + " nodes " + str(self.v_nodes) + " nps " + str(nps) + " pv " + str(local_move))
-=======
 class Board:
     board_state = []
     played_move_count = 0
@@ -671,16 +417,9 @@ class Search:
 
             # if get_perf_counter() >= self.end_time or v_depth < 1:
             #     break
->>>>>>> master
 
-            if get_perf_counter() >= self.end_time or v_depth < 1:
-                break
+        return [iterative_score, iterative_move]
 
-<<<<<<< HEAD
-        return [local_score, local_move]
-
-    def first_search(self, position, depth):
-=======
     def print_stats(self, v_depth, v_score, v_time, v_nodes, v_nps, v_pv):
         print_to_terminal("info depth " + v_depth + " score cp " + v_score + " time " + v_time + " nodes " + v_nodes + " nps " + v_nps + " pv " + v_pv)
 
@@ -723,23 +462,12 @@ class Search:
     def search(self, local_board, v_depth, alpha, beta):
         global_score = -1e8
         chosen_move = None
->>>>>>> master
 
         local_score = -1e8
 
         # alpha = -1e8
         # beta = 1e8
 
-<<<<<<< HEAD
-        local_score = -1e8
-        best_move = None
-
-        for s_move in sorted(position.gen_moves(), key=position.value, reverse=True):
-            self.v_nodes += 1
-            temp_score = self.pvs(position.move(s_move), -beta, -alpha, depth)
-
-            print(mrender(position, s_move), temp_score)
-=======
         is_white = local_board.played_move_count % 2 == 0
 
         v_depth = max(v_depth, 1)
@@ -755,34 +483,9 @@ class Search:
             if local_score >= global_score:
                 global_score = local_score
                 chosen_move = s_move
->>>>>>> master
 
-            if temp_score >= local_score:
-                local_score = temp_score
-                best_move = s_move
+        return [global_score, chosen_move]
 
-<<<<<<< HEAD
-        return [local_score, best_move]
-
-    def pvs(self, position, alpha, beta, depth):
-        if depth <= 0:
-            # if get_color(position):
-            return position.score
-            # return -position.score
-
-        alpha_orig = alpha
-
-        tt_entry = self.tt_lookup(position)
-        if tt_entry['tt_depth'] >= depth:
-            if tt_entry['tt_flag'] == EXACT:
-                return tt_entry['tt_value']
-            elif tt_entry['tt_flag'] == LOWER:
-                alpha = max(alpha, tt_entry['tt_value'])
-            elif tt_entry['tt_flag'] == UPPER:
-                beta = min(beta, tt_entry['tt_value'])
-
-            if alpha >= beta:
-=======
     def tt_lookup(self, local_board):
         board_string = local_board.str_board()
         if board_string not in self.tt_bucket:
@@ -824,19 +527,10 @@ class Search:
 
             if alpha >= beta:
                 self.v_nodes += 1
->>>>>>> master
                 return tt_entry['tt_value']
 
         local_score = -1e8
 
-<<<<<<< HEAD
-        for s_move in sorted(position.gen_moves(), key=position.value, reverse=True):
-            self.v_nodes += 1
-            local_score = -self.pvs(position.move(s_move), -alpha - 1, -alpha, depth - 1)
-            if local_score > alpha and local_score < beta:
-                local_score = -self.pvs(position.move(s_move), -beta, -local_score, depth - 1)
-
-=======
         for s_move in local_board.get_valid_moves():
             self.v_nodes += 1
 
@@ -844,7 +538,6 @@ class Search:
             if local_score > alpha and local_score < beta:
                 local_score = -self.pvs(local_board.make_move(s_move), -beta, -local_score, v_depth - 1)
 
->>>>>>> master
             alpha = max(alpha, local_score)
 
             if alpha >= beta:
@@ -857,76 +550,6 @@ class Search:
             tt_entry['tt_flag'] = LOWER
         else:
             tt_entry['tt_flag'] = EXACT
-<<<<<<< HEAD
-        tt_entry['tt_depth'] = depth
-        self.store_tt(position, tt_entry)
-
-        return alpha
-
-    def tt_lookup(self, position):
-        board_string = position.board + str(get_color(position))
-        if board_string not in self.tt_bucket:
-            self.tt_bucket[board_string] = {
-                'tt_depth': 0,
-                'tt_value': -1e5,
-                'tt_flag': 2
-            }
-        return self.tt_bucket[board_string]
-
-    def store_tt(self, position, tt_entry):
-        board_string = position.board + str(get_color(position))
-        if len(self.tt_bucket) > 1e7:
-            self.tt_bucket = {}
-        self.tt_bucket[board_string] = tt_entry
-
-WHITE = 0
-BLACK = 1
-
-UPPER = 1
-LOWER = 2
-EXACT = 3
-
-MATE_LOWER = piece['K'] - 10*piece['Q']
-MATE_UPPER = piece['K'] + 10*piece['Q']
-
-gc.enable()
-
-def parse(c):
-    fil, rank = ord(c[0]) - ord('a'), int(c[1]) - 1
-    return A1 + fil - 10*rank
-
-def mparse(color, move):
-    m = (parse(move[0:2]), parse(move[2:4]))
-    return m if color == WHITE else (119-m[0], 119-m[1])
-
-def render(i):
-    rank, fil = divmod(i - A1, 10)
-    return chr(fil + ord('a')) + str(-rank + 1)
-
-def mrender(pos, m):
-    # Sunfish always assumes promotion to queen
-    p = 'q' if A8 <= m[1] <= H8 and pos.board[m[0]] == 'P' else ''
-    m = m if get_color(pos) == WHITE else (119-m[0], 119-m[1])
-    return render(m[0]) + render(m[1]) + p
-
-def get_color(pos):
-    ''' A slightly hacky way to to get the color from a sunfish position '''
-    return BLACK if pos.board.startswith('\n') else WHITE
-
-def renderFEN(pos, half_move_clock=0, full_move_clock=1):
-    color = 'wb'[get_color(pos)]
-    if get_color(pos) == BLACK:
-        pos = pos.rotate()
-    board = '/'.join(pos.board.split())
-    board = re.sub(r'\.+', (lambda m: str(len(m.group(0)))), board)
-    castling = ''.join(itertools.compress('KQkq', pos.wc[::-1]+pos.bc)) or '-'
-    ep = render(pos.ep) if not pos.board[pos.ep].isspace() else '-'
-    clock = '{} {}'.format(half_move_clock, full_move_clock)
-    return ' '.join((board, color, castling, ep, clock))
-
-def main():
-    hist = [Position(initial, 0, (True, True), (True, True), 0, 0)]
-=======
         tt_entry['tt_depth'] = v_depth
         self.store_tt(local_board, tt_entry)
 
@@ -937,7 +560,6 @@ UPPER=2
 LOWER=3
 
 def main():
->>>>>>> master
     searcher = Search()
 
     game_board = Board()
@@ -949,36 +571,17 @@ def main():
             if line == "quit":
                 sys.exit()
             elif line == "uci":
-<<<<<<< HEAD
-                print("pygone 1.0 by rcostheta")
-                print("uciok")
-            elif line == "print":
-                print(renderFEN(hist[-1], len(hist), math.ceil(len(hist) / 2)))
-            elif line == "ucinewgame":
-                hist = [Position(initial, 0, (True, True), (True, True), 0, 0)]
-                # searcher.reset()
-                gc.collect()
-=======
                 print_to_terminal("pygone 1.0\nuciok")
             elif line == "ucinewgame":
                 game_board.reset()
                 searcher.reset()
->>>>>>> master
             elif line == "isready":
                 print_to_terminal("readyok")
             elif line.startswith("position"):
-                color = WHITE
                 moves = line.split()
-<<<<<<< HEAD
-                hist = [Position(initial, 0, (True, True), (True, True), 0, 0)]
-                for position_move in moves[3:]:
-                    hist.append(hist[-1].move(mparse(color, position_move)))
-                    color = 1 - color
-=======
                 game_board.reset()
                 for position_move in moves[3:]:
                     game_board = game_board.make_move(position_move)
->>>>>>> master
             elif line.startswith("go"):
                 white_time = 1e8
                 black_time = 1e8
@@ -996,23 +599,11 @@ def main():
                     elif arg == 'infinite':
                         input_depth = 30
 
-<<<<<<< HEAD
-                time_move_calc = max(40 - len(hist), 2)
-
-                time_move_calc = 40
-                if len(hist) > 38:
-                    time_move_calc = 2
-                else:
-                    time_move_calc = 40 - len(hist)
-
-                is_white = len(hist) % 2 == 0
-=======
                 time_move_calc = max(40 - game_board.played_move_count, 2)
 
                 move_time = 1e8
 
                 is_white = game_board.played_move_count % 2 == 0
->>>>>>> master
 
                 if is_white:
                     move_time = white_time / (time_move_calc * 1e3)
@@ -1032,15 +623,8 @@ def main():
 
                 (score, s_move) = searcher.iterative_search(game_board, go_depth, move_time)
 
-<<<<<<< HEAD
-                searcher.v_nodes = 0
-                searcher.v_tthits = 0
-                start_time = get_perf_counter()
-                (score, s_move) = searcher.iterative_search(hist[-1], go_depth, move_time)
-=======
                 (score, s_move) = searcher.search(game_board, go_depth, score - 10, score + 10)
 
->>>>>>> master
                 print_to_terminal("bestmove " + s_move)
         except (KeyboardInterrupt, SystemExit):
             print_to_terminal('quit')
