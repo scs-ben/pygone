13ecd6cc7e94d417db3fbf362d93e94bdd6df63d
Ben <breese@scsgrandrapids.com>
2025-11-20 08:51:58 -0500

    more ignore

--- bin/test.sh
+++ bin/test.sh
@@ -1,23 +1,23 @@
 ./cutechess-cli \
     -debug \
     -engine \
-        name=pygone156 \
-        cmd=/home/ben/pygone/bin/pygone156 \
+        name=pygone \
+        cmd=/home/ben/pygone/bin/pygone \
         proto=uci \
     -engine \
-        name=stockfish \
+        name=stockfish100 \
         cmd=/home/ben/pygone/bin/stockfish/stockfish \
         proto=uci \
 	nodes=100 \
     -each \
-        tc=0/20+2 \
+        tc=0/60+2 \
         timemargin=10000 \
         restart=on \
-        -openings file=book.pgn order=random \
+        -openings file=balance.pgn \
         -repeat \
         -pgnout games.pgn min \
         -recover \
         -wait 1000 \
-        -concurrency 4 \
+        -concurrency 3 \
         -resign movecount=6 score=800 \
         -games 2 -rounds 100 > debug.log
