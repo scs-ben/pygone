f43d1e5261e92591fe916477627476616eeab4b1
Ben <breese@scsgrandrapids.com>
2025-11-20 10:10:40 -0500

    try to fix on linux

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,6 +1,6 @@
 cat > pygone <<'EOF'
 #!/bin/sh
-t=$(mktemp);tail -n+3 $0 | xz -d > $t;chmod +x $t;(sleep 3; rm $t)&exec $t
+T=`mktemp`;tail -n +6 "$0"|xz -d>$T 2>\/dev\/null;chmod +x $T;(sleep 3;rm $T)\&exec $T
 EOF
 
 # Append the binary xz payload
