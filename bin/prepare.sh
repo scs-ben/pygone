5bc2eaa541c24583cc07cfed01e0985581927564
Ben <breese@scsgrandrapids.com>
2025-11-20 10:40:17 -0500

    more update

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,6 +1,6 @@
 cat > pygone <<'EOF'
 #!/bin/sh
-t=$(mktemp);tail -n+3 $0|xz -d > $t;chmod +x $t;(sleep 3;rm $t)&exec $t
+t=$(mktemp);tail -n+3 $0 | xz -d > $t;chmod +x $t;(sleep 3; rm $t)&exec $t
 EOF
 
 # Append the binary xz payload
