d7950cb1d2a08c4f70b2d418bd9fbd65eefd8113
rcostheta <reeseman04@gmail.com>
2026-06-09 22:38:46 -0400

    testing updates

--- test/reg_test.sh
+++ test/reg_test.sh
@@ -1,12 +1,12 @@
 ./cutechess-cli \
     -debug \
     -engine \
-        name=pygone \
+        name=pygone-latest \
         cmd=/home/ben/pygone/bin/pygone \
         proto=uci \
     -engine \
-        name=pygone165 \
-        cmd=/home/ben/pygone/bin/pygone165 \
+        name=pygone2 \
+        cmd=/home/ben/pygone/historical/pygone2-11b142 \
         proto=uci \
     -each \
         tc=0/30+2 \
@@ -17,6 +17,6 @@
         -pgnout games.pgn min \
         -recover \
         -wait 1000 \
-        -concurrency 2 \
+        -concurrency 1 \
         -resign movecount=6 score=800 \
         -games 2 -rounds 100 > debug.log
